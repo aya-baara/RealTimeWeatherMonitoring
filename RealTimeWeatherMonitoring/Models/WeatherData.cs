6ae9738ab893fd12f7d432c3e2361ef117818dec
ayabaara <ayabaara4@gmail.com>
2025-03-24 15:57:48 +0200

    implement WeatherData model

---
+++ RealTimeWeatherMonitoring/Models/WeatherData.cs
@@ -0,0 +1,15 @@
+﻿namespace RealTimeWeatherMonitoring.Models;
+class WeatherData
+{
+    public string Location { get; set; }
+    public double Temperature { get; set; }
+    public double Humidity { get; set; }
+
+    public WeatherData (string location, double temperature, double humidity)
+    {
+        Location = location;
+        Temperature = temperature;
+        Humidity = humidity;
+    }
+}
+
