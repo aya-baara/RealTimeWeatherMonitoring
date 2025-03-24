9287d5db2b691bdb9ee189998fca2f6344c92650
ayabaara <ayabaara4@gmail.com>
2025-03-24 16:06:00 +0200

    implement Bot model

---
+++ RealTimeWeatherMonitoring/Models/Bot.cs
@@ -0,0 +1,7 @@
+﻿namespace RealTimeWeatherMonitoring.Models;
+class Bot
+{
+    public bool Enabled { get; set; }
+    public double Threshold { get; set; }
+    public string Message { get; set; }
+}
