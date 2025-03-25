7ddacddeb28cf92cac0991767f5fac491813e907
ayabaara <ayabaara4@gmail.com>
2025-03-25 11:00:36 +0200

    implement WeatherDataReaderFactory

---
+++ RealTimeWeatherMonitoring/Services/WeatherDataReaderFactory.cs
@@ -0,0 +1,22 @@
+﻿using RealTimeWeatherMonitoring.Interfaces;
+
+namespace RealTimeWeatherMonitoring.Services;
+class WeatherDataReaderFactory
+{
+    public static IWeatherDataReader GetReader(string s)
+    {
+        if (s.StartsWith("{"))
+        {
+            return new JsonWeatherDataReader();
+
+        }
+        else if (s.StartsWith("<"))
+        {
+            return new XmlWeatherDataReader();
+        }
+        else
+        {
+            throw new Exception("Unsupported weather data format.");
+        }
+    }
+}
