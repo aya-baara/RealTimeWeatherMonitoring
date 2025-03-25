19fa74dae110c33ce80b89d592e5d608390d2135
ayabaara <ayabaara4@gmail.com>
2025-03-25 11:38:26 +0200

    implement WeatherBot interface

---
+++ RealTimeWeatherMonitoring/Interfaces/IWeatherBot.cs
@@ -0,0 +1,8 @@
+﻿using RealTimeWeatherMonitoring.Models;
+
+namespace RealTimeWeatherMonitoring.Interfaces;
+interface IWeatherBot
+{
+    public (bool, string) Update(WeatherData weatherData);
+}
+
