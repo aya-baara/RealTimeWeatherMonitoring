8141d8e070c5639f3d1715d1b7a631cb142836b3
ayabaara <ayabaara4@gmail.com>
2025-03-25 11:38:58 +0200

    implement RainBot

---
+++ RealTimeWeatherMonitoring/Services/Bots/RainBot.cs
@@ -0,0 +1,22 @@
+﻿using RealTimeWeatherMonitoring.Interfaces;
+using RealTimeWeatherMonitoring.Models;
+
+namespace RealTimeWeatherMonitoring.Services.Bots;
+class RainBot : IWeatherBot
+{
+    private BotConfig _BotConfig;
+
+    public RainBot(BotConfig botConfig)
+    {
+        _BotConfig = botConfig;
+    }
+    public (bool, string) Update(WeatherData weatherData)
+    {
+        if (_BotConfig.Enabled && weatherData.Humidity > _BotConfig.Threshold)
+        {
+            return (true, _BotConfig.Message);
+        }
+        return (false, "");
+    }
+}
+
