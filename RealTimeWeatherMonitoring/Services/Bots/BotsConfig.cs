914e0b501e905ca6d4fe72e5cd07143bb86b675b
ayabaara <ayabaara4@gmail.com>
2025-03-25 13:03:41 +0200

    implement botsconfig

---
+++ RealTimeWeatherMonitoring/Services/Bots/BotsConfig.cs
@@ -0,0 +1,27 @@
+﻿using RealTimeWeatherMonitoring.Models;
+
+namespace RealTimeWeatherMonitoring.Services.Bots;
+class BotsConfig
+{
+    public RainBot RainBot { get; private set; }
+    public SnowBot SnowBot { get; private set; }
+    public SunBot SunBot { get; private set; }
+
+    public BotsConfig() { }
+    public BotsConfig(Dictionary<WeatherBots, BotConfig> botsConfig)
+    {
+        botsConfig.TryGetValue(WeatherBots.RainBot, out BotConfig rainConfig);
+        RainBot = new RainBot(rainConfig!);
+
+        botsConfig.TryGetValue(WeatherBots.RainBot, out BotConfig snowConfig);
+        SnowBot = new SnowBot(snowConfig!);
+
+        botsConfig.TryGetValue(WeatherBots.RainBot, out BotConfig sunConfig);
+        SunBot = new SunBot(sunConfig!);
+
+
+
+    }
+
+}
+
