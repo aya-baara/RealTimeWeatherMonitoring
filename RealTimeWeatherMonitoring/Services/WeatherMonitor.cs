789e06bba8240d4a883cd1a71acd8a63a5eefcb3
ayabaara <ayabaara4@gmail.com>
2025-03-25 13:36:05 +0200

    Add WeatherMonitor class

---
+++ RealTimeWeatherMonitoring/Services/WeatherMonitor.cs
@@ -0,0 +1,42 @@
+﻿using RealTimeWeatherMonitoring.Interfaces;
+using RealTimeWeatherMonitoring.Models;
+using RealTimeWeatherMonitoring.Services.Bots;
+
+namespace RealTimeWeatherMonitoring.Services;
+class WeatherMonitor : IWeatherMonitor
+{
+    private List<IWeatherBot> _weatherBots = new List<IWeatherBot>();
+    public WeatherData CurrentWeatherData { get; private set; }
+
+    public WeatherMonitor(BotsConfig botsConfig)
+    {
+        _weatherBots.Add(botsConfig.SnowBot);
+        _weatherBots.Add(botsConfig.RainBot);
+        _weatherBots.Add(botsConfig.SunBot);
+
+    }
+    public void Attach(IWeatherBot bot)
+    {
+        _weatherBots.Add(bot);
+    }
+
+    public void Detach(IWeatherBot bot)
+    {
+        _weatherBots.Remove(bot);
+    }
+
+    public void Notify()
+    {
+        foreach (var bot in _weatherBots)
+        {
+            bot.Update(CurrentWeatherData);
+        }
+    }
+
+    public void UpdateWeather(WeatherData weatherData)
+    {
+        CurrentWeatherData = weatherData;
+        Notify();
+    }
+}
+
