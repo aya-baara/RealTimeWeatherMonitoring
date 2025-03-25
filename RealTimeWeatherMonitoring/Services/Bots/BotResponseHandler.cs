3e0cbb834d402354860422c3c2a6889162ff23d1
ayabaara <ayabaara4@gmail.com>
2025-03-25 11:56:52 +0200

    implement BotResponseHandler

---
+++ RealTimeWeatherMonitoring/Services/Bots/BotResponseHandler.cs
@@ -0,0 +1,12 @@
+﻿namespace RealTimeWeatherMonitoring.Services.Bots;
+class BotResponseHandler
+{
+    public static void HandleBotResponse((bool, string) response)
+    {
+        if (response.Item1)
+        {
+            Console.WriteLine(response.Item2);
+        }
+    }
+}
+
