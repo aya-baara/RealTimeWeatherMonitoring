5b815901d009645148c25b4fbcab622d2cbccc96
ayabaara <ayabaara4@gmail.com>
2025-03-25 13:36:46 +0200

    implement WeatherBotFactory

---
+++ RealTimeWeatherMonitoring/Services/WeatherBotFactory.cs
@@ -0,0 +1,13 @@
+﻿using RealTimeWeatherMonitoring.Services.Bots;
+
+namespace RealTimeWeatherMonitoring.Services;
+class WeatherBotFactory
+{
+    public static BotsConfig CreateBotsConfig(string filePath)
+    {
+        var botConfigData = new ConfigReader().ReadBotsConfig(filePath);
+
+        return new BotsConfig(botConfigData);
+    }
+}
+
