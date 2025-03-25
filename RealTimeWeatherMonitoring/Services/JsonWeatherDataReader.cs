6e6d53117bca6e423bbf01931052ad8b51095a90
ayabaara <ayabaara4@gmail.com>
2025-03-25 10:39:01 +0200

    implement Xml & Json reader

---
+++ RealTimeWeatherMonitoring/Services/JsonWeatherDataReader.cs
@@ -0,0 +1,13 @@
+﻿using RealTimeWeatherMonitoring.Interfaces;
+using RealTimeWeatherMonitoring.Models;
+using System.Text.Json;
+
+namespace RealTimeWeatherMonitoring.Services;
+class JsonWeatherDataReader : IWeatherDataReader
+{
+    public WeatherData ReadWeatherData(string jsonWeatherData)
+    {
+        return JsonSerializer.Deserialize<WeatherData>(jsonWeatherData);
+    }
+}
+
---
+++ RealTimeWeatherMonitoring/Services/XmlWeatherDataReader.cs
@@ -0,0 +1,15 @@
+﻿using RealTimeWeatherMonitoring.Interfaces;
+using RealTimeWeatherMonitoring.Models;
+using System.Xml.Serialization;
+
+namespace RealTimeWeatherMonitoring.Services;
+class XmlWeatherDataReader : IWeatherDataReader
+{
+    public WeatherData ReadWeatherData(string xmlWeatherData)
+    {
+        var serializer = new XmlSerializer(typeof(WeatherData), new XmlRootAttribute("Weather"));
+        using StringReader reader = new StringReader(xmlWeatherData);
+        return (WeatherData)serializer.Deserialize(reader);
+    }
+}
+
