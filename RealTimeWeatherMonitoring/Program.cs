e643448d788b26b0f5f1e08dfd73408fca4efae9
ayabaara <ayabaara4@gmail.com>
2025-03-25 14:22:42 +0200

    implement menu

--- RealTimeWeatherMonitoring/Program.cs
+++ RealTimeWeatherMonitoring/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using RealTimeWeatherMonitoring.Services;
+using RealTimeWeatherMonitoring.Services.Bots;
 Console.WriteLine("Hello, World!");
+
+// <<Json input example>>
+//{"Location": "City Name", "Temperature": 35.8, "Humidity": 20.4}
+
+
+//// <<XML input example>>
+//<Weather><Location>City Name</Location><Temperature>23.7</Temperature><Humidity>85.5</Humidity></Weather>
+
+Menu menu = new Menu();
---
+++ RealTimeWeatherMonitoring/Services/Menu.cs
@@ -0,0 +1,72 @@
+﻿
+using RealTimeWeatherMonitoring.Interfaces;
+using RealTimeWeatherMonitoring.Models;
+using RealTimeWeatherMonitoring.Services.WeatherReader;
+
+namespace RealTimeWeatherMonitoring.Services;
+class Menu
+{
+    private IWeatherDataReader weatherDataReader;
+    private WeatherMonitor weatherMonitor;
+
+    public void SetUp()
+    {
+        weatherMonitor = new WeatherMonitor(WeatherBotFactory.CreateBotsConfig(@"C:\Users\hp\source\repos\RealTimeWeatherMonitoring\RealTimeWeatherMonitoring\BotsConfig.json"));
+    }
+    public Menu()
+    {
+        SetUp();
+        DisplayMenu();
+    }
+    public void DisplayMenu()
+    {
+        while (true)
+        {
+            Console.WriteLine("1. Enter weather data");
+            Console.WriteLine("2. Exit");
+            Console.Write("Please select an option (1 or 2): ");
+
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int option))
+            {
+                switch (option)
+                {
+                    case 1:
+                        EnterWeatherData();
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Exiting program...");
+                        return;
+
+                    default:
+                        Console.WriteLine("Invalid option. Please select 1 or 2.");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a number.");
+            }
+        }
+    }
+
+    private void EnterWeatherData()
+    {
+        Console.WriteLine("Enter Weather Data as Json or Xml format .. send it in one line");
+        string weatherDataInput = Console.ReadLine();
+        try
+        {
+            weatherDataReader = WeatherDataReaderFactory.GetReader(weatherDataInput);
+            WeatherData inputWeatherData = weatherDataReader.ReadWeatherData(weatherDataInput);
+            weatherMonitor.UpdateWeather(inputWeatherData);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        
+    }
+}
+
