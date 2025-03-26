cc35740d6e12e7f39005657b2265395266ad6353
ayabaara <ayabaara4@gmail.com>
2025-03-26 10:40:46 +0200

    remove comments

--- RealTimeWeatherMonitoring/Program.cs
+++ RealTimeWeatherMonitoring/Program.cs
@@ -2,10 +2,5 @@
 
 Menu menu = new Menu();
 
-// <<Json input example>>
-//{"Location": "City Name", "Temperature": 35.8, "Humidity": 20.4}
 
-
-//// <<XML input example>>
-//<Weather><Location>City Name</Location><Temperature>23.7</Temperature><Humidity>85.5</Humidity></Weather>
 
