f8ba5e5e63a3514183c0e019b074c2fd9fa14a2b
ayabaara <ayabaara4@gmail.com>
2025-03-26 10:39:48 +0200

    remove constructors

--- RealTimeWeatherMonitoring/Models/WeatherData.cs
+++ RealTimeWeatherMonitoring/Models/WeatherData.cs
@@ -7,15 +7,5 @@ public class WeatherData
     public required double Temperature { get; set; }
     public required double Humidity { get; set; }
 
-    [SetsRequiredMembers]
-    public WeatherData(string location, double temperature, double humidity)
-    {
-        Location = location;
-        Temperature = temperature;
-        Humidity = humidity;
-    }
-
-    //XmlSerializer needs a parameterless constructor
-    public WeatherData() { }
 }
 
