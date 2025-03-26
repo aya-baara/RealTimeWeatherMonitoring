7bb0677a8a7ea7e6b7f9aee715db373d76facc76
ayabaara <ayabaara4@gmail.com>
2025-03-26 11:29:23 +0200

    make UpdateWeather method abstract

--- RealTimeWeatherMonitoring/Interfaces/IWeatherMonitor.cs
+++ RealTimeWeatherMonitoring/Interfaces/IWeatherMonitor.cs
@@ -1,8 +1,11 @@
-﻿namespace RealTimeWeatherMonitoring.Interfaces;
+﻿using RealTimeWeatherMonitoring.Models;
+
+namespace RealTimeWeatherMonitoring.Interfaces;
 interface IWeatherMonitor
 {
     void Attach(IWeatherBot bot);
     void Detach(IWeatherBot bot);
     void Notify();
+    public void UpdateWeather(WeatherData weatherData);
 }
 
