97dde57153e3ca158d61acc5196a6d70c426e871
ayabaara <ayabaara4@gmail.com>
2025-03-27 14:36:10 +0200

    Add method to handle single bot creation, handle missing configurations, and catch exceptions for missing config

--- RealTimeWeatherMonitoring/Services/Bots/BotsConfig.cs
+++ RealTimeWeatherMonitoring/Services/Bots/BotsConfig.cs
@@ -6,8 +6,17 @@ class BotsConfig
 {
     public IReadOnlyList<IWeatherBot> WeatherBots { get; private set; }
 
-    public BotsConfig() {
-        WeatherBots=WeatherBotFactory.GetBotConfigList(ConfigReader.ReadBotsConfig(FilePaths.ConfigFilePath));
+    public BotsConfig()
+    {
+        try
+        {
+            WeatherBots = WeatherBotFactory.GetBotConfigList(ConfigReader.ReadBotsConfig(FilePaths.ConfigFilePath));
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
     }
 
 
--- RealTimeWeatherMonitoring/Services/WeatherBotFactory.cs
+++ RealTimeWeatherMonitoring/Services/WeatherBotFactory.cs
@@ -5,19 +5,42 @@ using RealTimeWeatherMonitoring.Services.Bots;
 namespace RealTimeWeatherMonitoring.Services;
 class WeatherBotFactory
 {
+    public static IWeatherBot CreateBot(WeatherBots botType, BotConfig config)
+    {
+        switch (botType)
+        {
+            case WeatherBots.RainBot:
+                return new RainBot(config);
+            case WeatherBots.SnowBot:
+                return new SnowBot(config);
+            case WeatherBots.SunBot:
+                return new SunBot(config);
+            default:
+                throw new ArgumentException($"Unknown bot type: {botType}");
+        }
+    }
     public static List<IWeatherBot> GetBotConfigList(Dictionary<WeatherBots, BotConfig> botsConfig)
     {
-        var Bots = new List<IWeatherBot>();
-        botsConfig.TryGetValue(WeatherBots.RainBot, out BotConfig rainConfig);
-        Bots.Add(new RainBot(rainConfig!));
+        var bots = new List<IWeatherBot>();
 
-        botsConfig.TryGetValue(WeatherBots.SnowBot, out BotConfig snowConfig);
-        Bots.Add(new SnowBot(snowConfig!));
+        if (botsConfig.TryGetValue(WeatherBots.RainBot, out var rainConfig))
+            bots.Add(CreateBot(WeatherBots.RainBot, rainConfig));
+        else
+            throw new ArgumentException("Missing configuration for RainBot.");
 
-        botsConfig.TryGetValue(WeatherBots.SunBot, out BotConfig sunConfig);
-        Bots.Add(new SunBot(sunConfig!));
-        return Bots;
+        if (botsConfig.TryGetValue(WeatherBots.SnowBot, out var snowConfig))
+            bots.Add(CreateBot(WeatherBots.SnowBot, snowConfig));
+        else
+            throw new ArgumentException("Missing configuration for SnowBot.");
+
+        if (botsConfig.TryGetValue(WeatherBots.SunBot, out var sunConfig))
+            bots.Add(CreateBot(WeatherBots.SunBot, sunConfig));
+        else
+            throw new ArgumentException("Missing configuration for SunBot.");
+
+        return bots;
     }
+
 
 }
 
