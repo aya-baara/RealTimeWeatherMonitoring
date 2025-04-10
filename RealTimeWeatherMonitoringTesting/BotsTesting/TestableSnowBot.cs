d2e93449f052e64d69e922c79773aabac5e7ec73
ayabaara <ayabaara4@gmail.com>
2025-04-10 15:06:54 +0300

    test for SnowBot's IsSatisfyBotCondition

---
+++ RealTimeWeatherMonitoringTesting/BotsTesting/TestableSnowBot.cs
@@ -0,0 +1,13 @@
+﻿using RealTimeWeatherMonitoring.Models;
+using RealTimeWeatherMonitoring.Services.Bots;
+
+namespace RealTimeWeatherMonitoringTesting.BotsTesting;
+public class TestableSnowBot : SnowBot
+{
+    public TestableSnowBot(BotConfig botConfig) : base(botConfig) { }
+
+    public bool CallIsSatisfyBotCondition(WeatherData weatherData)
+    {
+        return base.IsSatisfyBotCondition(weatherData);
+    }
+}
