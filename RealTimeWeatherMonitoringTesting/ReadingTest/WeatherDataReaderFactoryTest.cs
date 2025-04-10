514774d7b1d7761ece97bf93bbc5ea3634456e68
ayabaara <ayabaara4@gmail.com>
2025-04-10 16:09:55 +0300

    test weatherDataReader

---
+++ RealTimeWeatherMonitoringTesting/ReadingTest/WeatherDataReaderFactoryTest.cs
@@ -0,0 +1,28 @@
+﻿using RealTimeWeatherMonitoring.Services.WeatherReader;
+namespace RealTimeWeatherMonitoringTesting.ReadingTest;
+public class WeatherDataReaderFactoryTest
+{
+    [Fact]
+    public void GetReader_ReturnsJsonReader_WhenInputStartsWithCurlyBrace()
+    {
+        var reader = WeatherDataReaderFactory.GetReader("{ \"Temperature\": 25, \"Humidity\": 50 }");
+        Assert.IsType<JsonWeatherDataReader>(reader);
+    }
+
+    [Fact]
+    public void GetReader_ReturnsXmlReader_WhenInputStartsWithAngle()
+    {
+        var reader = WeatherDataReaderFactory.GetReader("<Weather><Temperature>25</Temperature><Humidity>50</Humidity></Weather>");
+        Assert.IsType<XmlWeatherDataReader>(reader);
+    }
+
+    [Fact]
+    public void GetReader_ThrowExeption_WhenInputDoesNotStartWithJsonOrXmlFormat()
+    {
+        Assert.Throws<Exception>(() => WeatherDataReaderFactory.GetReader("[ ]"));
+    }
+
+
+
+}
+
