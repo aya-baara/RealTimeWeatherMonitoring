ffc7788d3550cfde1826759ffc178e585196d3c8
ayabaara <ayabaara4@gmail.com>
2025-04-13 11:10:12 +0300

    Test Xml  Reading

---
+++ RealTimeWeatherMonitoringTesting/ReadingTest/XmlWeatherDataReaderTest.cs
@@ -0,0 +1,28 @@
+﻿using RealTimeWeatherMonitoring.Models;
+using RealTimeWeatherMonitoring.Services.WeatherReader;
+
+namespace RealTimeWeatherMonitoringTesting.ReadingTest;
+
+public class XmlWeatherDataReaderTest
+{
+    [Fact]
+    public void Read_ValidJson_ReturnsCorrectWeatherData()
+    {
+        var xml = @"<Weather>
+                <Location>Palestine</Location>
+                <Temperature>35.5</Temperature>
+                <Humidity>70</Humidity>
+                </Weather>";
+
+        var reader = new XmlWeatherDataReader();
+
+        WeatherData result = reader.Read(xml);
+
+
+        Assert.NotNull(result);
+        Assert.Equal("Palestine", result.Location);
+        Assert.Equal(35.5, result.Temperature);
+        Assert.Equal(70, result.Humidity);
+    }
+}
+
