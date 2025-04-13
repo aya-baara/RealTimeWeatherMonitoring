b300ef7d7ee26b4bf135763f68b2cf0630e6922f
ayabaara <ayabaara4@gmail.com>
2025-04-13 10:49:32 +0300

    Testing WeatherBots initialize

---
+++ RealTimeWeatherMonitoringTesting/BotsTesting/BotsConfigTest.cs
@@ -0,0 +1,15 @@
+﻿using RealTimeWeatherMonitoring.Services.Bots; 
+using RealTimeWeatherMonitoring; 
+namespace RealTimeWeatherMonitoringTesting.BotsTesting;
+public class BotsConfigTest
+{
+    [Fact]
+    public void Constructor_ShouldInitializeWeatherBots_FromConfigFile()
+    {
+        FilePaths.LoadConfiguration();
+        BotsConfig bots = new BotsConfig();
+        Assert.NotNull(bots.WeatherBots);
+    }
+
+}
+
