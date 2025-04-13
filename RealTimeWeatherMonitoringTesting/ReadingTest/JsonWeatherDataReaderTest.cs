8e2661dab83e170b3b12edd9a26acf45c15a2e0c
ayabaara <ayabaara4@gmail.com>
2025-04-13 10:56:05 +0300

    Test Json Reading

---
+++ RealTimeWeatherMonitoringTesting/ReadingTest/JsonWeatherDataReaderTest.cs
@@ -0,0 +1,28 @@
+﻿using RealTimeWeatherMonitoring.Models;
+using RealTimeWeatherMonitoring.Services.WeatherReader;
+
+namespace RealTimeWeatherMonitoringTesting.ReadingTest;
+public class JsonWeatherDataReaderTest
+{
+    [Fact]
+    public void Read_ValidJson_ReturnsCorrectWeatherData()
+    {
+
+        var json = @"{
+                ""Location"": ""Palestine"",
+                ""Temperature"": 35.5,
+                ""Humidity"": 70
+            }";
+
+        var reader = new JsonWeatherDataReader();
+
+        WeatherData result = reader.Read(json);
+
+
+        Assert.NotNull(result);
+        Assert.Equal("Palestine", result.Location);
+        Assert.Equal(35.5, result.Temperature);
+        Assert.Equal(70, result.Humidity);
+    }
+}
+
