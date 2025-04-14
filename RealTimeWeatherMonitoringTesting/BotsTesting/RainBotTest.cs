8fb9882fbf1c41391e37c565f28566c9da96fde2
ayabaara <ayabaara4@gmail.com>
2025-04-14 18:07:27 +0300

    Extract common fixture instance to the constructor

--- RealTimeWeatherMonitoringTesting/BotsTesting/RainBotTest.cs
+++ RealTimeWeatherMonitoringTesting/BotsTesting/RainBotTest.cs
@@ -1,35 +1,36 @@
 using AutoFixture;
 using RealTimeWeatherMonitoring.Models;
-using RealTimeWeatherMonitoring.Services.Bots;
 
 namespace RealTimeWeatherMonitoringTesting.BotsTesting
 {
     public class RainBotTest
     {
+        private readonly Fixture fixture;
+
+        RainBotTest()
+        {
+            fixture = new Fixture();
+        }
+
         [Theory]
         [InlineData(50, true)]
         [InlineData(30, false)]
         public void IsSatisfyRainBotCondition_ReturnsExpectedResult_BasedOnHumidity(int weatherHumidity, bool expected)
         {
-            var fixture = new Fixture();
-
             BotConfig botConfig = fixture.Build<BotConfig>().With(b => b.Threshold, 40).Create();
             TestableRainBot rainBot = new TestableRainBot(botConfig);
-   
+
             WeatherData weather = fixture.Build<WeatherData>().With(b => b.Humidity, weatherHumidity).Create();
             bool IsSatisfying = rainBot.CallIsSatisfyBotCondition(weather);
 
             Assert.Equal(expected, IsSatisfying);
-
         }
 
         [Theory]
-        [InlineData (true,true)]
+        [InlineData(true, true)]
         [InlineData(false, false)]
-        public void UpdateTest_BasedOnBotEnable(bool enabled , bool expected)
+        public void UpdateTest_BasedOnBotEnable(bool enabled, bool expected)
         {
-            var fixture = new Fixture();
-
             BotConfig botConfig = fixture.Build<BotConfig>().With(b => b.Enabled, enabled).With(b => b.Threshold, 40).Create();
             TestableRainBot rainBot = new TestableRainBot(botConfig);
 
@@ -37,7 +38,6 @@ namespace RealTimeWeatherMonitoringTesting.BotsTesting
 
             var response = rainBot.Update(weather);
             Assert.Equal(expected, response.IsActivated);
-
 
         }
     }
