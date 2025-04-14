0a3a009ade8d0ed3e8bfe58481387a2f266b7264
ayabaara <ayabaara4@gmail.com>
2025-04-14 19:04:04 +0300

    extract commomn JsonWeatherDataReader definition to the contructor

--- RealTimeWeatherMonitoringTesting/ReadingTest/JsonWeatherDataReaderTest.cs
+++ RealTimeWeatherMonitoringTesting/ReadingTest/JsonWeatherDataReaderTest.cs
@@ -5,6 +5,12 @@ using System.Text.Json;
 namespace RealTimeWeatherMonitoringTesting.ReadingTest;
 public class JsonWeatherDataReaderTest
 {
+    private readonly JsonWeatherDataReader reader;
+
+    JsonWeatherDataReaderTest()
+    {
+        reader = new JsonWeatherDataReader();
+    }
     [Fact]
     public void Read_ValidJson_ReturnsCorrectWeatherData()
     {
@@ -14,7 +20,6 @@ public class JsonWeatherDataReaderTest
                 ""Humidity"": 70
             }";
 
-        var reader = new JsonWeatherDataReader();
         WeatherData result = reader.Read(json);
 
         Assert.NotNull(result);
@@ -26,8 +31,6 @@ public class JsonWeatherDataReaderTest
     [Fact]
     public void Read_NotValidJson_ReturnsNull()
     {
-        var reader = new JsonWeatherDataReader();
-
         Assert.Throws<JsonException>(() => reader.Read("not a json"));
     }
 }
